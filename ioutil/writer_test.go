b0d0923b665911ea49d6f0d6a8241cad90de3d34
Joe Tsai <joetsai@digital-static.net>
2015-07-08 20:56:45 -0700

    Add writer test

--- ioutil/writer_test.go
+++ ioutil/writer_test.go
@@ -5,12 +5,94 @@
 package ioutil_test
 
 import "io"
+import "os"
 import "testing"
 import "github.com/stretchr/testify/assert"
 import . "bitbucket.org/rawr/golib/ioutil"
 
 // TODO(jtsai): Finish this!
 func TestWriterOperations(t *testing.T) {
-	_ = io.Writer(NewWriter(nil))
-	assert.True(t, true)
+	var rd *Reader
+	var wr *Writer
+	var pos int64
+	var cnt64 int64
+	var cnt int
+	var err error
+	data := make([]byte, 64)
+
+	// Empty writer
+	wr = NewWriter(nil)
+
+	assert.Equal(t, wr.Len(), 0)
+
+	cnt, err = wr.Write(data)
+	assert.Equal(t, cnt, 0)
+	assert.Equal(t, err, io.ErrShortWrite)
+
+	cnt, err = wr.WriteAt(data, -1)
+	assert.Equal(t, cnt, 0)
+	assert.NotEqual(t, err, nil)
+
+	cnt, err = wr.WriteAt(data, 1)
+	assert.Equal(t, cnt, 0)
+	assert.Equal(t, err, io.ErrShortWrite)
+
+	err = wr.WriteByte(127)
+	assert.Equal(t, err, io.ErrShortWrite)
+
+	cnt, err = wr.WriteRune(12345)
+	assert.Equal(t, cnt, 0)
+	assert.Equal(t, err, io.ErrShortWrite)
+
+	rd = NewReader([]byte("Hello, world!"))
+	cnt64, err = wr.ReadFrom(rd)
+	assert.Equal(t, cnt64, 0)
+	assert.Equal(t, err, nil)
+
+	pos, err = wr.Seek(0, os.SEEK_SET)
+	assert.Equal(t, pos, 0)
+	assert.Equal(t, err, nil)
+
+	pos, err = wr.Seek(0, os.SEEK_CUR)
+	assert.Equal(t, pos, 0)
+	assert.Equal(t, err, nil)
+
+	pos, err = wr.Seek(0, os.SEEK_END)
+	assert.Equal(t, pos, 0)
+	assert.Equal(t, err, nil)
+
+	pos, err = wr.Seek(-1, os.SEEK_SET)
+	assert.Equal(t, pos, 0)
+	assert.NotEqual(t, err, nil)
+
+	pos, err = wr.Seek(-1, os.SEEK_CUR)
+	assert.Equal(t, pos, 0)
+	assert.NotEqual(t, err, nil)
+
+	pos, err = wr.Seek(-1, os.SEEK_END)
+	assert.Equal(t, pos, 0)
+	assert.NotEqual(t, err, nil)
+
+	pos, err = wr.Seek(+1, os.SEEK_SET)
+	assert.Equal(t, pos, 1)
+	assert.Equal(t, err, nil)
+
+	pos, err = wr.Seek(+1, os.SEEK_CUR)
+	assert.Equal(t, pos, 2)
+	assert.Equal(t, err, nil)
+
+	pos, err = wr.Seek(+1, os.SEEK_END)
+	assert.Equal(t, pos, 1)
+	assert.Equal(t, err, nil)
+
+	assert.Equal(t, wr.Len(), 0)
+
+	cnt, err = wr.Write(data)
+	assert.Equal(t, cnt, 0)
+	assert.Equal(t, err, io.ErrShortWrite)
+
+	// Actual writer
+	wr = NewWriter(make([]byte, 32))
+
+	assert.Equal(t, wr.Len(), 0)
 }
