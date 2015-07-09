a2bcfbcbeac7866d935f799116f727003e45fd49
Joe Tsai <joetsai@digital-static.net>
2015-07-08 20:38:05 -0700

    Add skeleton test files

---
+++ ioutil/util_test.go
@@ -0,0 +1,13 @@
+// Copyright 2014, Joe Tsai. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE.md file.
+
+package ioutil_test
+
+import "testing"
+
+// TODO(jtsai): Finish this!
+func TestReaderAtSize(t *testing.T) {}
+
+// TODO(jtsai): Finish this!
+func TestSeekerSize(t *testing.T) {}
