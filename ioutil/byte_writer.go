d0305400cbdee7e0c7784ff70f987634114bc014
Joe Tsai <joetsai@digital-static.net>
2015-08-13 03:07:25 -0700

    Trivial year change

--- ioutil/byte_writer.go
+++ ioutil/byte_writer.go
@@ -1,4 +1,4 @@
-// Copyright 2014, Joe Tsai. All rights reserved.
+// Copyright 2015, Joe Tsai. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE.md file.
 
