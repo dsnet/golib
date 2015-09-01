3bb9e66006f9a9cc04b56a6947b7afd9c205f61f
Joe Tsai <joetsai@digital-static.net>
2015-09-01 01:31:25 -0700

    Delete doc.go

--- doc.go
+++
@@ -1,6 +0,0 @@
-// Copyright 2014, Joe Tsai. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE.md file.
-
-// Pacakge golib is a collection of unrelated libraries.
-package golib
