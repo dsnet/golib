48a1ab7c1126eebff062b90ea712ecd9f817c8c4
Joe Tsai <joetsai@digital-static.net>
2016-01-22 14:46:33 -0800

    ioutil: modified package docstring

--- ioutil/util.go
+++ ioutil/util.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE.md file.
 
-// Package ioutil is a collection of small io related implementations.
+// Package ioutil is a collection of io related functionality.
 package ioutil
 
 import "io"
